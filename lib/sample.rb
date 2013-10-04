851fa687d6a03abf4c8ba29f9b135e5d254bed98
Sarat Kongara <sarat.kongara@invilogic.com>
2013-10-04 11:52:03 +0530

    Added a class for welcome message

---
+++ lib/sample.rb
@@ -0,0 +1,15 @@
+module Greetings
+  class Hello
+    def initialize(message)
+      @message = message
+    end
+    
+    def greet_user(name)
+      puts "#{@message} #{name}"
+    end
+  end
+  
+end
+
+welcome_message = Greetings::Hello.new("welcome")
+welcome_message.greet_user("sarat")
